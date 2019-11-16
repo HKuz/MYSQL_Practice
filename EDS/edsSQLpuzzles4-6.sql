794c89397722cb732872ef4432de8cc644e78358
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-11-16 10:34:41 -0500

    feat: update load data code to handle missing values

--- EDS/edsSQLpuzzles4-6.sql
+++ EDS/edsSQLpuzzles4-6.sql
@@ -2,12 +2,32 @@
 Database: edsSQLChalls
 
 Puzzles 4-6 use the secondary car market dataset
-Table: [ ]
+Table: carmkt
 */
 
+-- Create the table based on secondary_car_market.csv
+CREATE TABLE carmkt (
+    maker varchar(15),
+    model varchar(20),
+    mileage double unsigned,
+    manufacture_year int unsigned,
+    transmission varchar(5),
+    door_count float,
+    seat_count float,
+    displacement float,
+    horse_power float,
+    price double unsigned
+);
 
--- Create the table from secondary_car_market.csv
-
+-- Load data from secondary_car_market.csv
+LOAD DATA INFILE '/Users/heatherkusmierz/MySQLData/secondary_car_market.csv'
+INTO TABLE carmkt
+FIELDS TERMINATED BY ","
+(maker, model, mileage, manufacture_year, transmission,
+    @vdoor_count, @vseat_count, displacement, horse_power, price)
+SET
+door_count = nullif(@vdoor_count, ''),
+seat_count = nullif(@vseat_count, '');
 
 
 /*
