e645dfa2a9dd397cdf3975da4eae8175e9da0cbf
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-11-15 21:09:53 -0500

    feat: break challenge groups into separate files

---
+++ EDS/edsSQLpuzzles4-6.sql
@@ -0,0 +1,31 @@
+/*
+Database: edsSQLChalls
+
+Puzzles 4-6 use the secondary car market dataset
+Table: [ ]
+*/
+
+
+-- Create the table from secondary_car_market.csv
+
+
+
+/*
+PUZZLE 4
+
+*/
+
+
+
+/*
+PUZZLE 5
+
+*/
+
+
+
+/*
+PUZZLE 6
+
+*/
+
---
+++ EDS/edsSQLpuzzles7-9.sql
@@ -0,0 +1,30 @@
+/*
+Database: edsSQLChalls
+
+Puzzles 7-9 use the foot-moneyball dataset
+Table: [ ]
+*/
+
+-- Create the table from foot-moneyball.csv and quarterbacks.csv
+
+
+
+/*
+PUZZLE 7
+
+*/
+
+
+
+/*
+PUZZLE 8
+
+*/
+
+
+
+/*
+PUZZLE 9
+
+*/
+
