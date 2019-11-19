7f54448463452565dec86227c85b9d9e1364b872
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-11-18 19:47:03 -0500

    feat: add create table statement

--- EDS/edsSQLpuzzles7-9.sql
+++ EDS/edsSQLpuzzles7-9.sql
@@ -1,12 +1,32 @@
 /*
 Database: edsSQLChalls
 
-Puzzles 7-9 use the foot-moneyball dataset
-Table: [ ]
+Puzzles 7-9 use the foot-moneyball datasets
+Tables: [ ]
 */
 
 -- Create the table from foot-moneyball.csv and quarterbacks.csv
-
+CREATE TABLE foot_moneyball (
+    season int unsigned,
+    "date" date,
+    name varchar(20),
+    team char(3),
+    team_score int unsigned,
+    opponent char(3),
+    opponent_score int unsigned,
+    location char(1),
+    passing_attempts int unsigned,
+    completions int unsigned,
+    interceptions int unsigned,
+    passer_rating float,
+    sacks int unsigned,
+    sacks_yards_lost int unsigned,
+    passing_touchdowns int unsigned,
+    passing_yards int unsigned,
+    rushing_attempts int unsigned,
+    rushing_touchdowns int unsigned,
+    rushing_yards int unsigned
+);
 
 
 /*
