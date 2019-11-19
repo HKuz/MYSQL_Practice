155f2833b35f702211fcde5931810345f9bf775f
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-11-19 08:14:45 -0500

    feat: start EDS SQL challenge puzzle 7

--- EDS/edsSQLpuzzles7-9.sql
+++ EDS/edsSQLpuzzles7-9.sql
@@ -51,9 +51,35 @@ draft_round = nullif(@vdraft_round, '');
 
 /*
 PUZZLE 7
+Write a query that:
 
+- Calculates the average score allowed by a "team" to "opponents" in 2012, in
+their home stadium (i.e. location = 'H')
+- Calculates the average score allowed by an "opponent" to "teams" in 2012, in
+their home stadium (i.e. location = 'A')
+- Selects any teams for which the above calculations do not match.
+
+Tip: Remember to account for the possibility of multiple quarterbacks on a team playing in a game.
 */
 
+SELECT
+    t_home.*, o_away.avg_team_score
+FROM (
+    SELECT
+        team, AVG(opponent_score) AS avg_op_score
+    FROM foot_moneyball
+    WHERE YEAR(date)=2012 AND location='H'
+    GROUP BY team
+) t_home
+JOIN (
+    SELECT
+        opponent, AVG(team_score) AS avg_team_score
+    FROM foot_moneyball
+    WHERE YEAR(date)=2012 AND location='A'
+    GROUP BY opponent
+) o_away
+ON t_home.team = o_away.opponent
+WHERE t_home.avg_op_score <> o_away.avg_team_score;
 
 
 /*
